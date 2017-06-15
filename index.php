073a1d3570d6509b79d50f313671c003c4478328
Kennet Skov Olesen <kenn799e@edu.campusvejle.dk>
2017-06-15 09:49:43 +0200

    started på eksammen opgaven

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSS/styles.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <hr>
+            <nav>
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="#">PLANTER</a></li>
+                    <li><a href="#">INDRETNING</a></li>
+                    <li><a href="Login.html">LOG IND</a></li>
+                </ul>
+            </nav>
+            <hr> </header>
+        <main> </main>
+        <footer></footer>
+    </div>
+</body>
+
+</html>
